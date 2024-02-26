e91679969359dfa736c9919d7d54c5c532d98403
Marek Holba <marekholba@gmail.com>
2024-02-26 16:49:25 +0100

    (2)_ autorization

---
+++ StagApi/Services/TokenService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StagApi.Services
+{
+    public class TokenService
+    {
+        static string CreateAccessToken(JwtOptions jwtOptions, string userId, TimeSpan expiration, string[] permissions)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(jwtOptions.SigningKey);
+            var symmetricKey = new SymmetricSecurityKey(keyBytes);
+
+            var signingCredentials = new SigningCredentials(
+                symmetricKey,
+                // 👇 one of the most popular. 
+                SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, userId),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)),
+                new(ClaimTypes.NameIdentifier, userId),
+            };
+
+            var roleClaims = permissions.Select(x => new Claim("role", x));
+            claims.AddRange(roleClaims);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtOptions.Issuer,
+                audience: jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.Now.Add(expiration),
+                signingCredentials: signingCredentials);
+
+            var rawToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return rawToken;
+        }
+        public static async Task<IResult> Connect(string userId, string password, JwtOptions jwtOptions)
+        {
+            //creates the access token (jwt token)
+            var tokenExpiration = TimeSpan.FromSeconds(jwtOptions.ExpirationSeconds);
+            var accessToken = CreateAccessToken(
+                jwtOptions,
+                userId,
+                TimeSpan.FromMinutes(60),
+                new[] { "read_todo", "create_todo" });
+
+            //returns a json response with the access token
+            return Results.Ok(new
+            {
+                access_token = accessToken,
+                expiration = (int)tokenExpiration.TotalSeconds,
+                type = "bearer"
+            });
+        }
+    }
+}
